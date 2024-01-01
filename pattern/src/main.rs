f5c584a0ee0403460441ca98361f6f1daeb77485
prashantsharma <sharmaprash2207@gmail.com>
2024-01-01 14:16:38 +0530

    increasing triangles

--- pattern/src/main.rs
+++ pattern/src/main.rs
@@ -11,9 +11,11 @@ fn main() {
             0 as u32
         }
     };
-    // solid_cube(length);
-    // increasing_rows_columns(length);
-    triangle(length);
+    // _solid_cube(length);
+    // _increasing_cube(length);
+    // _triangle(length);
+    // _increasing_triangle(length);
+    increasing_triangle_2(length);
 }
 
 fn _solid_cube(l: u32) -> i32 {
@@ -32,7 +34,7 @@ fn _solid_cube(l: u32) -> i32 {
 //  # #
 //  # # #
 //  # # # #
-fn triangle(l: u32) -> i32 {
+fn _triangle(l: u32) -> i32 {
     for i in 1..l+1 {
         for _j in 0..i {
             print!("# ")
@@ -48,7 +50,7 @@ fn triangle(l: u32) -> i32 {
 // 5 6 7 8
 // 9 10 11 12
 // 13 14 15 16
-fn _increasing_rows_columns(l: u32) -> i32 {
+fn _increasing_cube(l: u32) -> i32 {
     for i in 0..l {
         for j in 0..l {
             print!("{} ", j + (l * i) + 1)
@@ -59,4 +61,35 @@ fn _increasing_rows_columns(l: u32) -> i32 {
 }
 
 
+// for l = 4 => 
+//  1
+//  2 3
+//  4 5 6
+//  7 8 9 10
+fn _increasing_triangle(l: u32) -> i32 {
+    let mut c = 0;
+    for i in 1..l+1 {
+        for _j in 0..i {
+            c += 1;
+            print!("{c} ")
+        }
+        println!("")
+    }
+    return 0;
+}
+// for l = 4 => 
+//  1
+//  2 3
+//  4 5 6
+//  7 8 9 10
+fn increasing_triangle_2(l: u32) -> i32 {
+    for i in 1..l+1 {
+        let start = (i * (i - 1 )) / 2 + 1;
+        for j in 0..i {
+            print!("{} ", start + j )
+        }
+        println!("")
+    }
+    return 0;
+}
 
